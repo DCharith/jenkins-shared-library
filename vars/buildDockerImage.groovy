f703532391b9d0877d011fad092c9d715c03a334
unknown <dilvirgil@gmail.com>
2021-11-27 15:48:46 +0530

    Initial commit

---
+++ vars/buildDockerImage.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+def call(){
+    echo "Building Docker image..."
+    withCredentials([usernamePassword(credentialsId: "dockerhub-credentials", usernameVariable: "username", passwordVariable: "password")]){
+        sh "docker build -t dcharith/mobile-app-ws:2.0 ."
+        sh "echo $password | docker login -u $username --password-stdin"
+        sh "docker push dcharith/mobile-app-ws:2.0"
+    }
+}
