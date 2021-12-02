49a48e83e8b9058ad0f46c1cfb5c9f16f35bc57c
unknown <dilvirgil@gmail.com>
2021-12-03 00:25:29 +0530

    added image name as a veriable

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call(){
+def call(String imageName){
     echo "Building Docker image..."
     withCredentials([usernamePassword(credentialsId: "dockerhub-credentials", usernameVariable: "username", passwordVariable: "password")]){
-        sh "docker build -t dcharith/mobile-app-ws:2.0 ."
+        sh "docker build -t $imageName ."
         sh "echo $password | docker login -u $username --password-stdin"
-        sh "docker push dcharith/mobile-app-ws:2.0"
+        sh "docker $imageName"
     }
 }
